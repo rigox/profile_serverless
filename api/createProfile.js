ad5ea4fc9bc6e8f9e477ad501d99e18f2abf1124
Rigoberto Contreras <rigoc1994@gmail.com>
2020-05-27 23:48:20 -0400

    added createProfile

---
+++ api/createProfile.js
@@ -0,0 +1,20 @@
+const  colors =  require("colors")
+
+
+
+//@Desc  creates a profile
+//@Route POST profile/
+//@Access private
+
+exports.handler =  async(event) =>{
+  
+
+    try {
+        console.log('console.log'.green.inverse)
+
+    } catch (err) {
+         console.log(err)
+    }
+     
+
+}
