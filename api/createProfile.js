2bbc1bff5958b5ed3a72def1db847660077cd854
Rigoberto Contreras <rigoc1994@gmail.com>
2020-06-01 23:37:18 -0400

    finallyt

--- api/createProfile.js
+++ api/createProfile.js
@@ -1,17 +1,21 @@
 const  colors =  require("colors")
-
+const mongoose =  require("mongoose")
 
 
 //@Desc  creates a profile
 //@Route POST profile/
 //@Access private
 
-exports.handler =  async(event) =>{
+exports.handler =  async (event,context,cb) =>{
   
+let   temp = {
+     data : event.body
+}
 
+temp = JSON.stringify(temp)
     try {
-        console.log('console.log'.green.inverse)
-
+        console.log('console.log here is the mongoose', mongoose)
+        cb(temp)
     } catch (err) {
          console.log(err)
     }
